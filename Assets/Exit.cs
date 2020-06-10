20f8fea90b828f5f41564657cdb3863834880a18
KarinaQA7174 <66557247+KarinaQA7174@users.noreply.github.com>
2020-06-10 14:27:12 +0300

    Окончательная корректировка

---
+++ Assets/Exit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Exit : MonoBehaviour
+{
+    public Button exitButton;
+
+    void TaskOnClick()
+    {
+        Application.Quit();
+    }
+
+    void Start()
+    {
+        exitButton.onClick.AddListener (TaskOnClick);
+    }
+}
